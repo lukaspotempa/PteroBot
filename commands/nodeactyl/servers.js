a52f0a5a08625ef1bd300a6c49c40758c73a563b
Lukas Potempa <potempa.lukas.paul@gmail.com>
2023-04-16 18:42:02 +0200

    Added /servers command * /servers lists all deployed and non-suspended servers.

---
+++ commands/nodeactyl/servers.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, Embed, EmbedBuilder } = require('discord.js');
+const Nodeactyl = require('nodeactyl');
+const { API_Key, API_Url, pterodactyl_img, interface_name } = require('../../config.json');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('servers')
+    .setDescription('Lists all not suspended servers.'),
+
+  async execute(interaction) {
+    try {
+      const application = new Nodeactyl.NodeactylApplication(API_Url, API_Key);
+      const json = await application.getAllServers();
+      const embed = new EmbedBuilder()
+        .setColor('#FF0000')
+        .setTitle('All non-suspended servers:')
+        .setURL(API_Url)
+        .setAuthor({ name: interface_name + '-Bot', iconURL: pterodactyl_img, url: API_Url })
+        .setThumbnail(pterodactyl_img)
+        .setTimestamp()
+        .setFooter({ text: 'Bot created by Avoid#6906' });
+        json.data.forEach(server => {
+          const status = server.attributes.suspended ? 'suspended' : 'not suspended';
+          if (server.attributes.suspended) return;
+          embed.addFields(
+            { name: server.attributes.name, value:
+              `ID: ${server.attributes.id}
+              description: ${server.attributes.description}`,
+            },
+          );
+        });
+      return interaction.reply({ embeds: [ embed ] });
+    } catch (error) {
+      console.error(error);
+      await interaction.reply('An error occurred while fetching the JSON response. Please contact the server owner.');
+    }
+  },
+};
+
--- index.js
+++ index.js
@@ -31,7 +31,6 @@ client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
 	const command = interaction.client.commands.get(interaction.commandName);
-    const { commandName } = interaction;
 
 	if (!command) {
 		console.error(`No command matching ${interaction.commandName} was found.`);
