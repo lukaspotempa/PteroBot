c3663c13987585de03d2d1aa5dc6b71ad72b6c1e
Lukas Potempa <potempa.lukas.paul@gmail.com>
2023-04-17 11:37:49 +0200

    /stop [server_id] added * Will stop a server instance.

---
+++ commands/nodeactyl/stop.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, blockQuote } = require('discord.js');
+const Nodeactyl = require('nodeactyl');
+const { Client_API_Key, API_Key, API_Url, pterodactyl_img } = require('../../config.json');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('stop')
+    .setDescription('Stops a server instance.')
+    .addStringOption(option =>
+		option.setName('server_id')
+			.setDescription('The server id. Can be retrieved through the panel or using /servers command.')
+            .setRequired(true)),
+
+  async execute(interaction) {
+    try {
+        const server_id = interaction.options.getString('server_id');
+        const application = new Nodeactyl.NodeactylApplication(API_Url, API_Key);
+        const client = new Nodeactyl.NodeactylClient(API_Url, Client_API_Key);
+        const data = await application.getServerDetails(server_id);
+        await client.stopServer(data.identifier);
+        const message = blockQuote(`Server ${data.name} will stop shortly. In case server doesn't stop, use /kill.`);
+
+        return interaction.reply({ content: message });
+    } catch (error) {
+      console.error(error);
+      await interaction.reply('An error occurred while fetching the JSON response. Please contact the server owner.');
+    }
+  },
+};
+
