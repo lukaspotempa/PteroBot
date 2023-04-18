464a19da6cf12a6394f40265026d543c7c5d868f
Lukas Potempa <potempa.lukas.paul@gmail.com>
2023-04-18 13:23:22 +0200

    bug fix on server monitoring * Bug caused duplication of red and green circle

--- monitor/server_monitor.js
+++ monitor/server_monitor.js
@@ -1,7 +1,7 @@
 const Nodeactyl = require('nodeactyl');
 const gamedig = require('gamedig');
-const { ChannelManager } = require('discord.js');
-const { guildId } = require('../config.json');
+const { ChannelManager, EmbedBuilder } = require('discord.js');
+const { guildId, pterodactyl_img, clientId } = require('../config.json');
 
 module.exports = {
 	async execute(client, game_servers) {
@@ -9,19 +9,38 @@ module.exports = {
 			game_servers.forEach(async server => {
 				const channel = client.channels.cache.find(c => c == server.channel_id);
 				let channel_name = channel.name;
+				let server_data = 'offline';
 				try {
-					const server_data = await gamedig.query({
-					type: server.game_type,
-					host: server.host,
-					port: server.query_port,
-				});
-				channel_name = `🟢᲼${ server_data.name }`;
+					server_data = await gamedig.query({
+						type: server.game_type,
+						host: server.host,
+						port: server.query_port,
+					});
+					channel_name = `🟢᲼${ server_data.name }`;
 				} catch (error) {
-					channel_name = `🔴᲼${ channel_name }`;
+					if (channel_name.includes('🟢')) {
+						channel_name = channel_name.replace('🟢', '🔴');
+					} else if (!channel_name.includes('🔴')) {
+						channel_name = `🔴᲼${ channel_name }`;
+					}
 				}
 				channel.setName(channel_name);
+				const messages = await channel.messages.fetch({ limit: 1 });
+				const message = messages.first();
+				const embed = new EmbedBuilder()
+				.setColor('#FF0000')
+				.setTitle(channel_name)
+				.setThumbnail(pterodactyl_img)
+				.setTimestamp()
+				.setFooter({ text: 'Bot by Avoid#6906' });
+				if (message) {
+					if (message.author.id == clientId) {
+						return await message.edit({ embeds: [ embed ] });
+					}
+				} else {
+					return await channel.send({ embeds: [ embed ] });
+				}
 			});
-			return console.log('success');
 		} catch (error) {
 			return console.error(error);
 		}
